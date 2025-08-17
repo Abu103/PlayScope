fd5898f697b9f26caa0ac72b332452fef9a009d9
Abu Nasar <abunasar103@gmail.com>
2025-08-17 19:11:43 +0530

    Fix css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { goto } from '$app/navigation';
+	import Spinner from '$lib/components/Spinner.svelte';
 
 	let { children } = $props();
 	let inputValue: string = $state('');
+	let loading: boolean = $state(false);
 
 	function gotoHome() {
 		goto('/', { replaceState: false });
@@ -13,7 +15,7 @@
 	function handleInputSubmit(event: SubmitEvent) {
 		event.preventDefault();
 		console.log('INSIDE');
-
+		loading = true;
 		goto(`/search/${inputValue}`, { replaceState: false });
 		inputValue = '';
 	}
@@ -23,14 +25,18 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+{#await loading}
+	<Spinner/>
+{:then loading } 
+	
 <div
-	class="z-40 overflow-hidden md:block lg:fixed lg:top-0 lg:w-full lg:bg-black/80 lg:px-5 lg:py-7 lg:text-lg lg:text-white lg:backdrop-blur-sm"
+class="z-40 overflow-hidden md:block lg:fixed lg:top-0 lg:w-full lg:bg-black/80 lg:px-5 lg:py-7 lg:text-lg lg:text-white lg:backdrop-blur-sm"
 >
-	<div class="flex items-center justify-between gap-3">
-		<div class="flex items-center gap-10">
-			<a class="logo w-fit max-w-2xs " onclick={gotoHome} href="/">
-				<svg
-					viewBox="0 0 240 60"
+<div class="flex items-center justify-between gap-3">
+	<div class="flex items-center gap-10">
+		<a class="logo w-fit max-w-2xs" onclick={gotoHome} href="/">
+			<svg
+			viewBox="0 0 240 60"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
 					style="width: 100%; height: 100%; display: block;"
@@ -44,36 +50,36 @@
 						<circle cx="20" cy="10" r="3" fill="#0F172A" />
 						<rect x="12" y="18" width="6" height="6" fill="#0F172A" />
 					</g>
-
+					
 					<!-- Site Name Text -->
 					<text
-						x="60"
-						y="38"
-						font-family="Segoe UI, Arial, sans-serif"
+					x="60"
+					y="38"
+					font-family="Segoe UI, Arial, sans-serif"
 						font-size="20"
 						fill="#f1f1f1"
 						font-weight="600"
 						class="hidden lg:block"
-					>
+						>
 						PlayScope
 					</text>
 				</svg>
 			</a>
 			<form onsubmit={handleInputSubmit}>
 				<input
-					class=" w-fit rounded-full px-3 py-1  outline-2 outline-offset-0 outline-yellow-300 focus-visible:outline-[#FACC15] lg:min-w-md"
+					class=" w-fit rounded-full px-5 py-1 outline-2 outline-offset-0 outline-yellow-300 focus-visible:outline-[#FACC15] lg:min-w-md"
 					type="text"
 					placeholder="Search"
 					bind:value={inputValue}
-				/>
-			</form>
-		</div>
-		<ul class="flex gap-7">
-			<li class="flex place-self-center rounded-sm px-2 py-1 hover:bg-[#FACC15] hover:text-black">
-				<a href="/" aria-label={'Home'} onclick={gotoHome}>Home</a>
-			</li>
-			<li class="flex place-self-center rounded-sm px-3 py-2 hover:bg-[#FACC15] hover:text-black">
-				Catalog
+					/>
+				</form>
+			</div>
+			<ul class="flex gap-7">
+				<li class="flex place-self-center rounded-sm px-3 py-2 hover:bg-[#FACC15] hover:text-black">
+					<a href="/" aria-label={'Home'} onclick={gotoHome}>Home</a>
+				</li>
+				<li class="flex place-self-center rounded-sm px-3 py-2 hover:bg-[#FACC15] hover:text-black">
+					Catalog
 			</li>
 			<li class="flex place-self-center rounded-sm px-3 py-2 hover:bg-[#FACC15] hover:text-black">
 				About us
@@ -85,6 +91,7 @@
 	</div>
 </div>
 {@render children?.()}
+{/await}
 
 <style>
 	ul > li:hover {
--- src/routes/search/[slug]/+page.svelte
+++ src/routes/search/[slug]/+page.svelte
@@ -14,25 +14,27 @@
 	});
 
 	function gotoPage(slug: string) {
-        goto(`/${slug}`, {replaceState:false})
-    }
+		goto(`/${slug}`, { replaceState: false });
+	}
 </script>
 
-<div class="bg-gray-900 px-5 pt-0">
-	<div class="columns-1 gap-4 bg-gray-900 py-10 sm:columns-2 lg:columns-3">
+<div class="flex place-self-center bg-gray-900 px-10 pt-0 lg:pt-30 w-full">
+	<div class="columns-1 gap-4  py-5 sm:columns-2 lg:columns-3">
 		{#each data.game.results as item (item.id)}
-			<a
-				href={`/${item.slug}`}
-				onclick={() => {
-					gotoPage(item.slug);
-				}}
-				class="my-4 h-fit break-inside-avoid rounded-sm bg-gray-800 p-3"
-			>
-				{#if item.background_image}
-					<img src={item.background_image} alt={item.name} class="rounded-sm" />
-				{/if}
-				<p class="pt-3 text-lg font-semibold text-white">{item.name}</p>
-			</a>
+			<div class=" flex my-5 rounded-sm hover:border-[#FACC15] border-2 hover:rounded-sm">
+				<a
+					href={`/${item.slug}`}
+					onclick={() => {
+						gotoPage(item.slug);
+					}}
+					class=" h-fit break-inside-avoid rounded-sm bg-gray-900 p-3"
+				>
+					{#if item.background_image}
+						<img src={item.background_image} alt={item.name} class="rounded-sm" />
+					{/if}
+					<p class="pt-3 text-lg font-semibold text-white">{item.name}</p>
+				</a>
+			</div>
 		{/each}
 	</div>
 </div>
